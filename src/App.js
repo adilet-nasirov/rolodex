9604695cfdac427a5c884bc7f3f45eebcfe05641
Adilet Nasirov <adilet.nasirov@yahoo.com>
2022-04-23 14:53:21 -0700

    fetch from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {React, useEffect, useState} from 'react'
 function App() {
+  const [monsters, setMonsters]= useState([]);
+  useEffect(()=>{
+    const data= fetch('https://jsonplaceholder.typicode.com/users').then(responce=> responce.json()).then(d=>{
+      setMonsters(d)
+    })
+  },[])
   return (
     <div className="App">
+      {
+        monsters.map(monster=>{
+          return <h1 key={monster.id}>{monster.name}</h1>
+        })
+      }
     </div>
   );
 }
